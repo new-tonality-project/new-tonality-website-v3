76f0fdb5561c6e50504677fa4bf570c1e743cd07
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-08-19 19:32:02 +0200

    adds user button to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
-import { usePathname } from 'next/navigation'
 import { Container } from '../Container'
-import { clamp } from '@/lib'
-import { Avatar, AvatarContainer } from './Avatar'
+import { Logo } from './Logo'
 import { MobileNavigation } from './MobileNavigation'
 import { DesktopNavigation } from './DesktopNavigation'
 import { NAV_ITEMS } from '@/lib/const'
@@ -15,116 +12,9 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
-import { Button } from '../Button'
 import { HeaderButton } from './HeaderButton'
 
 export function Header() {
-  let isHomePage = usePathname() === '/'
-
-  let headerRef = useRef<React.ElementRef<'div'>>(null)
-  let avatarRef = useRef<React.ElementRef<'div'>>(null)
-  let isInitial = useRef(true)
-
-  useEffect(() => {
-    let downDelay = avatarRef.current?.offsetTop ?? 0
-    let upDelay = 64
-
-    function setProperty(property: string, value: string) {
-      document.documentElement.style.setProperty(property, value)
-    }
-
-    function removeProperty(property: string) {
-      document.documentElement.style.removeProperty(property)
-    }
-
-    function updateHeaderStyles() {
-      if (!headerRef.current) {
-        return
-      }
-
-      let { top, height } = headerRef.current.getBoundingClientRect()
-      let scrollY = clamp(
-        window.scrollY,
-        0,
-        document.body.scrollHeight - window.innerHeight,
-      )
-
-      if (isInitial.current) {
-        setProperty('--header-position', 'sticky')
-      }
-
-      setProperty('--content-offset', `${downDelay}px`)
-
-      if (isInitial.current || scrollY < downDelay) {
-        setProperty('--header-height', `${downDelay + height}px`)
-        setProperty('--header-mb', `${-downDelay}px`)
-      } else if (top + height < -upDelay) {
-        let offset = Math.max(height, scrollY - upDelay)
-        setProperty('--header-height', `${offset}px`)
-        setProperty('--header-mb', `${height - offset}px`)
-      } else if (top === 0) {
-        setProperty('--header-height', `${scrollY + height}px`)
-        setProperty('--header-mb', `${-scrollY}px`)
-      }
-
-      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
-        setProperty('--header-inner-position', 'fixed')
-        removeProperty('--header-top')
-        removeProperty('--avatar-top')
-      } else {
-        removeProperty('--header-inner-position')
-        setProperty('--header-top', '0px')
-        setProperty('--avatar-top', '0px')
-      }
-    }
-
-    function updateAvatarStyles() {
-      if (!isHomePage) {
-        return
-      }
-
-      let fromScale = 1
-      let toScale = 36 / 64
-      let fromX = 0
-      let toX = 2 / 16
-
-      let scrollY = downDelay - window.scrollY
-
-      let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale
-      scale = clamp(scale, fromScale, toScale)
-
-      let x = (scrollY * (fromX - toX)) / downDelay + toX
-      x = clamp(x, fromX, toX)
-
-      setProperty(
-        '--avatar-image-transform',
-        `translate3d(${x}rem, 0, 0) scale(${scale})`,
-      )
-
-      let borderScale = 1 / (toScale / scale)
-      let borderX = (-toX + x) * borderScale
-      let borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`
-
-      setProperty('--avatar-border-transform', borderTransform)
-      setProperty('--avatar-border-opacity', scale === toScale ? '1' : '0')
-    }
-
-    function updateStyles() {
-      updateHeaderStyles()
-      updateAvatarStyles()
-      isInitial.current = false
-    }
-
-    updateStyles()
-    window.addEventListener('scroll', updateStyles, { passive: true })
-    window.addEventListener('resize', updateStyles)
-
-    return () => {
-      window.removeEventListener('scroll', updateStyles)
-      window.removeEventListener('resize', updateStyles)
-    }
-  }, [isHomePage])
-
   return (
     <header
       className="pointer-events-none relative z-50 flex flex-none flex-col"
@@ -134,7 +24,6 @@ export function Header() {
       }}
     >
       <div
-        ref={headerRef}
         className="top-0 z-10 h-16 pt-6"
         style={{
           position: 'var(--header-position)' as React.CSSProperties['position'],
@@ -149,9 +38,7 @@ export function Header() {
         >
           <div className="relative flex gap-4">
             <div className="flex flex-1">
-              <AvatarContainer>
-                <Avatar />
-              </AvatarContainer>
+              <Logo />
             </div>
             <div className="flex flex-1 justify-end md:justify-center">
               <MobileNavigation
@@ -164,7 +51,7 @@ export function Header() {
               />
             </div>
             <div className="flex justify-end md:flex-1">
-              <div className="pointer-events-auto flex gap-3">
+              <div className="w pointer-events-auto flex gap-3">
                 <SignedOut>
                   <SignInButton>
                     <HeaderButton variant="secondary">Sign In</HeaderButton>
@@ -174,7 +61,13 @@ export function Header() {
                   </SignUpButton>
                 </SignedOut>
                 <SignedIn>
-                  <UserButton />
+                  <UserButton
+                    appearance={{
+                      elements: {
+                        avatarBox: '!w-10 !h-10',
+                      },
+                    }}
+                  />
                 </SignedIn>
               </div>
             </div>
