dccb83a3fd700a7237b2230d862bce37a0acc7e4
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-08-20 08:00:22 +0200

    adds placeholder for the dissonance tool

--- src/app/tools/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from '@/components'
-
-export default function ToolsPage() {
-  return <Container className="mt-9">List tools here</Container>
-}
---
+++ src/app/tools/sethares-dissonance/page.tsx
@@ -0,0 +1,5 @@
+import { Container } from '@/components'
+
+export default function SetharesDissoanancePage() {
+  return <Container className="mt-18">Sethares dissonance</Container>
+}
