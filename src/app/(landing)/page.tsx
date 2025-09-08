44a6db6cf126a9161b36f09efc3f8002528c12e3
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-09-08 20:27:44 +0200

    adds section about web tools

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <>
       <Container className="py-30">
-        <div className="flex flex-col md:flex-row w-full justify-between gap-16">
+        <div className="flex w-full flex-col justify-between gap-16 md:flex-row">
           <div className="flex max-w-md flex-grow flex-col justify-center">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
               New Tonality
@@ -46,7 +46,34 @@ export default async function Home() {
         </div>
       </Container>
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2"></div>
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-x-10 gap-y-20 lg:max-w-none lg:grid-cols-3">
+          <div className='py-8'>
+            <h2 className="text-xl font-bold">Microtonal web apps</h2>
+            <p className="py-8">
+              Educate yourself on different concepts in psychoacoustics and
+              microtonality by playing with a set of tools that run on your
+              browser. Consider creating an account for the best experience.
+            </p>
+          </div>
+
+          <div>
+            <article className="rounded-lg border-1 border-zinc-100 px-6 py-8 shadow-lg shadow-zinc-800/5 md:mx-0 md:max-w-xs">
+              <h3 className="pb-4 text-xl">Dissonance perception survey</h3>
+              <p className="pb-2 font-thin">
+                Go thorugh the actual psychoacoustic experiment that was first
+                outlined in 1960s paper by Plomp and Levelt and that is the
+                basis behind modern empirical understanding of dissoannce.
+              </p>
+              <Link
+                href="/"
+                className="mt-2 flex items-center gap-1 text-amber-600"
+              >
+                Explore
+                <ChevronRightIcon className="h-4 w-4 stroke-current" />
+              </Link>
+            </article>
+          </div>
+        </div>
       </Container>
     </>
   )
