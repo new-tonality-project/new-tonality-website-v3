ca2beae527ef68d0399901190e353efd27955070
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-09-20 11:07:51 +0200

    adds page for dissonance survey

---
+++ src/app/tools/dissonance-survey/page.tsx
@@ -0,0 +1,12 @@
+import { SimpleLayout } from '@/components'
+
+export default async function DissonanceSurveyPage() {
+  return (
+    <SimpleLayout
+      title="Dissonance survey"
+      intro="Description here"
+    >
+      <p>Test</p>
+    </SimpleLayout>
+  )
+}
