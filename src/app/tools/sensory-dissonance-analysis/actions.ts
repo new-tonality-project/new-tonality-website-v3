a158150180631d4caba4ca8a14e62107ee4ff4ec
Seva Deriushkin <seva.deriushkin@gmail.com>
2026-03-07 18:36:49 +0100

    updates all dissonance graphs with new musical background

--- src/app/tools/sensory-dissonance-analysis/actions.ts
+++ src/app/tools/sensory-dissonance-analysis/actions.ts
@@ -33,6 +33,7 @@ export async function submitSurvey(args: {
         isMusician: args.musicalBackground === MusicalBackground.Musician,
         isNaiveListener:
           args.musicalBackground === MusicalBackground.NaiveListener,
+        userBackground: args.musicalBackground,
         shareDataPrivately: args.shareDataPrivately,
         shareDataPublicly: args.shareDataPublicly,
         createdAt: Date.now(),
@@ -44,18 +45,46 @@ export async function submitSurvey(args: {
   )
 
   const dissonanceGraphId = id()
-  
+
   await db.transact(
     db.tx.dissonanceGraphs[dissonanceGraphId]
       .create({
         createdAt: Date.now(),
         updatedAt: Date.now(),
+        userBackground: args.musicalBackground,
         meanFrequency: args.meanFrequency,
       })
       .link({
         $users: args.userId,
       }),
   )
+
+
+
+  const usersDissonanceGraphs = await db.queryOnce({
+    dissonanceGraphs: {
+      $: {
+        where: {
+          $users: args.userId,
+        },
+      },
+    },
+  })
+
+  if (usersDissonanceGraphs.data?.dissonanceGraphs.length !== 0) {
+    const transactionBatch1 = []
+
+
+    for (const graph of usersDissonanceGraphs.data?.dissonanceGraphs) {
+      transactionBatch1.push(
+        db.tx.dissonanceGraphs[graph.id].update({
+          userBackground: args.musicalBackground,
+        }),
+      )
+    }
+
+    await db.transact(transactionBatch1)
+  }
 
   const transactionBatch = []
 
